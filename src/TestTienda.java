e1c8f56abc3eb08e2ccba29edb92bbef5dc0a100
nicolasgcode <contactnicolasgarcia@gmail.com>
2024-08-16 15:13:18 -0300

    validaciones según producto funcionando

--- src/TestTienda.java
+++ src/TestTienda.java
@@ -1,6 +1,25 @@
+import logica.Producto;
+import logica.ProductoBebida;
+import logica.ProductoEnvasado;
+import logica.ProductoLimpieza;
 
 public class TestTienda {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        }
+
+//        Producto prod = new Producto("55555", "desc", 5, 3, 6, true );
+
+//        ProductoBebida p3 = new ProductoBebida("AC123", "Desc3", 11, 3.5, 8.1, true, 6.6, true );
+//
+//        ProductoEnvasado p1 = new ProductoEnvasado("AB123", "desc", 5, 3.1, 7.01, true, "vidrio", false);
+//
+//        ProductoLimpieza p2 = new ProductoLimpieza("AZ123", "Limpiador", 10, 15.5, 20.0, true, "COCINA");
+
+//        System.out.println("El producto envasado es: " + p1.toString());
+//        System.out.println("El producto bebida es: " + p3.toString());
+//        System.out.println("El producto limpieza es: " + p2.toString());
+
+
+    }
+
+
     }
--- src/logica/ProductoEnvasado.java
+++ src/logica/ProductoEnvasado.java
@@ -8,9 +8,47 @@ public class ProductoEnvasado extends Producto {
 
     public ProductoEnvasado(String id, String descripcion, int stock, double precioUnidad, double porcGanancia, boolean disponibleParaVenta, String tipoEnvase, boolean esImportado) {
         super(id,descripcion, stock, precioUnidad,porcGanancia, disponibleParaVenta );
+        if (this.idValido(id)) {
+            this.id = id;
+        } else {
+            System.out.println("Id invalido");
+        }
         this.tipoEnvase = tipoEnvase;
         this.esImportado = esImportado;
     }
 
+    public String getTipoEnvase() {
+        return tipoEnvase;
+    }
 
+    public void setTipoEnvase(String tipoEnvase) {
+        this.tipoEnvase = tipoEnvase;
+    }
+
+    public boolean isEsImportado() {
+        return esImportado;
+    }
+
+    public void setEsImportado(boolean esImportado) {
+        this.esImportado = esImportado;
+    }
+
+    @Override
+    public boolean idValido(String id){
+        return super.idValido(id) && id.matches("AB\\d{3}");
+    };
+
+    @Override
+    public String toString() {
+        return "ProductoEnvasado{" +
+                "tipoEnvase='" + tipoEnvase + '\'' +
+                ", esImportado=" + esImportado +
+                ", id='" + id + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", stock=" + stock +
+                ", precioUnidad=" + precioUnidad +
+                ", porcGanancia=" + porcGanancia +
+                ", disponibleParaVenta=" + disponibleParaVenta +
+                '}';
+    }
 }
